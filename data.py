d3df1b711602f5be8a8a3b96a6ac60dfe7594f41
Ali Zare <alizare0502@gmail.com>
2024-12-28 19:40:31 +0330

    added create_dataloader function.

--- data.py
+++ data.py
@@ -181,7 +181,93 @@ def create_food25(root:str):
 
 
 
+def create_dataloader(train_path:str,
+                      val_path:str,
+                      test_path:str,
+                      train_transform:transforms.Compose,
+                      test_transform:transforms.Compose,
+                      batch_size:int,
+                      num_workers:int):
+    """Creates training,validation and testing DataLoaders.
 
+  Takes in a training,validation and testing directory path and turns
+  them into PyTorch Datasets and then into PyTorch DataLoaders.
+
+  Args:
+    train_dir: Path to training directory.
+    val_dir:  Path to validation directory.
+    test_dir: Path to testing directory.
+    train_transform: torchvision transforms to perform on training data.
+    test_transform: torchvision transforms to perform on val and testing data.
+    batch_size: Number of samples per batch in each of the DataLoaders.
+    num_workers: An integer for number of workers per DataLoader.
+
+  Returns:
+    A tuple of (train_dataloader,val_dataloader, test_dataloader, class_names).
+    Where class_names is a list of the target classes.
+    Example usage:
+      train_dataloader, test_dataloader, class_names = \
+        = create_dataloaders(train_dir=path/to/train_dir,
+                             test_dir=path/to/test_dir,
+                             transform=some_transform,
+                             batch_size=32,
+                             num_workers=4)
+  """
+    train_data = datasets.ImageFolder(train_path, transform=train_transform)
+    val_data = datasets.ImageFolder(val_path, transform=test_transform)
+    test_data = datasets.ImageFolder(test_path, transform=test_transform)
+
+    # Get class names
+    class_names = train_data.classes
+
+    # Turn images into data loaders
+    train_dataloader = DataLoader(
+        train_data,
+        batch_size=batch_size,
+        shuffle=True,
+        num_workers=num_workers,
+        pin_memory=True,
+    )
+    # Turn images into data loaders
+    val_dataloader = DataLoader(
+        val_data,
+        batch_size=batch_size,
+        shuffle=False,
+        num_workers=num_workers,
+        pin_memory=True,
+    )
+
+    test_dataloader = DataLoader(
+        test_data,
+        batch_size=batch_size,
+        shuffle=False, # don't need to shuffle test data
+        num_workers=num_workers,
+        pin_memory=True,
+    )
+
+    return train_dataloader,val_dataloader, test_dataloader, class_names
+    
+
+
+
+
+
+
+
+def walk_through_dir(dir_path):
+  """
+  Walks through dir_path returning its contents.
+  Args:
+    dir_path (str or pathlib.Path): target directory
+
+  Returns:
+    A print out of:
+      number of subdiretories in dir_path
+      number of images (files) in each subdirectory
+      name of each subdirectory
+  """
+  for dirpath, dirnames, filenames in os.walk(dir_path):
+    print(f"There are {len(dirnames)} directories and {len(filenames)} images in '{dirpath}'.")
     
 
 
