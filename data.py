ff61fb83d88dccef94f5bf326e73075c09c18f5b
Ali Zare <alizare0502@gmail.com>
2024-12-28 15:20:50 +0330

    required packages added.

---
+++ data.py
@@ -0,0 +1,9 @@
+import torchvision.datasets as datasets
+import torchvision.transforms as transforms
+from torchvision.datasets import Food101
+import random
+from torch.utils.data import DataLoader
+import os
+from pathlib import Path
+import shutil
+
