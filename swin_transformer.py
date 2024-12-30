b9b251e40b97eca5ccd2da796326970233ad3cd4
Ali Zare <alizare0502@gmail.com>
2024-12-30 12:26:05 +0330

    create a file to train and eval swin_b transformer on food25 dataset.

---
+++ swin_transformer.py
@@ -0,0 +1,69 @@
+from data import download_food101,create_food25,create_dataloader,save_result
+import torchvision.transforms as transforms
+import os
+from torchvision import models
+import torch.nn as nn
+from torch.optim import Adam
+import engine
+import torch
+
+food_101_train_path,food_101_val_path,food_101_test_path=download_food101(root='data')
+#create food25
+train_path,val_path,test_path=create_food25(root='data')
+
+#define train and test transformer
+train_transform=transforms.Compose([
+    transforms.Resize((246, 246),interpolation=transforms.InterpolationMode.BICUBIC),
+    transforms.CenterCrop(size=(224,)),
+    transforms.ToTensor(),
+    transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+])
+
+test_transform=transforms.Compose([
+    transforms.Resize((224, 224),interpolation=transforms.InterpolationMode.BICUBIC),
+    transforms.ToTensor(),
+    transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+])
+
+train_dataloader,val_dataloader,test_dataloader,class_names=create_dataloader(train_path=train_path,
+                                                                              val_path=val_path,
+                                                                              test_path=test_path,
+                                                                              train_transform=train_transform,
+                                                                              test_transform=test_transform,
+                                                                              batch_size=64,
+                                                                              num_workers=os.cpu_count())
+
+#define the model
+model=models.swin_v2_b(weights='DEFAULT')
+model.head=nn.Linear(in_features=1024,out_features=25,bias=True)
+
+loss_fn=nn.CrossEntropyLoss()
+optimizer=Adam(params=model.parameters(),lr=0.001)
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+#path to save model and its results
+save_path='swin_b_food25_v1'
+results=engine.train(model=model,
+                     train_dataloader=train_dataloader,
+                     test_dataloader=val_dataloader,
+                     optimizer=optimizer,
+                     loss_fn=loss_fn,
+                     epochs=60,
+                     device=device,
+                     model_name="swin_b_food25_v1",
+                     model_saving_dir=save_path)
+
+test_results=engine.eval_model(model=model,
+                               dataloader=test_dataloader,
+                               loss_fn=loss_fn,
+                               num_classes=len(class_names),
+                               device=device)
+
+
+save_result(data_dict=results,
+            result_save_dir=save_path,
+            result_name="swin_b_food25_v1_train_results")
+save_result(data_dict=test_results,
+            result_save_dir=save_path,
+            result_name="swin_b_food25_v1_eval_results")
