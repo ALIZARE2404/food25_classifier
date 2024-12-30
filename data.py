ecd684be59f9091e8b8e224e26e559586d869d60
Ali Zare <alizare0502@gmail.com>
2024-12-30 12:50:54 +0330

    modified download_food101 function. that first check the existance of data then download it.

--- data.py
+++ data.py
@@ -25,124 +25,126 @@ def download_food101(root:str,
 
     """
     
-    
-    food101_train = Food101(root=root, split='train', download=True)
-    food101_test = Food101(root=root, split='test', download=True)
-    food_classes=food101_train.classes
-
-    if val_split:
-        food_101_path='data/food-101/images'
-        train_path = 'data/food-101/train'
-        val_path='data/food-101/val'
-        test_path = 'data/food-101/test'
-        
-        food_classes.sort()
-
-        if os.path.isdir(train_path) and os.path.isdir(test_path) and os.path.isdir(val_path):
-            print('train, val and test dir exist')
-            return(train_path,val_path,test_path)
-        else :
-            # Create directories for train and test
-            os.makedirs(train_path, exist_ok=True)
-            os.makedirs(test_path, exist_ok=True)
-            os.makedirs(val_path, exist_ok=True)
-
-
-
-            for food_class in food_classes:
-                src_class_path = os.path.join(food_101_path, food_class)
-
-                if os.path.isdir(src_class_path):
-                    # List all images in the class directory
-                    images = os.listdir(src_class_path)
-
-                    train_images = random.sample(images,750)  # First 750 for training
-                    remained_images = [item for item in images if item not in train_images]   # Remaining for testing
-                    val_image_number=int((val_pecentage*1000)/100)
-                    val_images=remained_images[:val_image_number]
-                    test_images=remained_images[val_image_number:]
-
-                    # Create class directories in train and test folders
-                    os.makedirs(os.path.join(train_path, food_class), exist_ok=True)
-                    os.makedirs(os.path.join(val_path, food_class), exist_ok=True)
-                    os.makedirs(os.path.join(test_path, food_class), exist_ok=True)
-
-                    # Copy training images
-                    for image_name in train_images:
-                        shutil.copy(os.path.join(src_class_path, image_name),
-                                    os.path.join(train_path, food_class))
-                    # Copy validation images
-                    for image_name in val_images:
-                        shutil.copy(os.path.join(src_class_path, image_name),
-                                    os.path.join(val_path, food_class))
-
-                    # Copy testing images
-                    for image_name in test_images:
-                        shutil.copy(os.path.join(src_class_path, image_name),
-                                    os.path.join(test_path, food_class))
-                    shutil.rmtree(src_class_path)
-
-            print("Train,val and test datasets created successfully.")
-
-            shutil.rmtree(food_101_path)
-            shutil.rmtree('data/food-101/meta')
-            os.remove("data/food-101/README.txt")
-            os.remove("data/food-101/license_agreement.txt")
-            os.remove("data/food-101.tar.gz")
-
-            return (train_path,val_path,test_path)
-    
+    if os.path.isdir(f"{root}/food-101"):
+        print("food101 downloaded")
     else:
-        food_101_path='data/food-101/images'
-        train_path = 'data/food-101/train'
-        test_path = 'data/food-101/test'
+        food101_train = Food101(root=root, split='train', download=True)
+        food101_test = Food101(root=root, split='test', download=True)
+        food_classes=food101_train.classes
+
+        if val_split:
+            food_101_path='data/food-101/images'
+            train_path = 'data/food-101/train'
+            val_path='data/food-101/val'
+            test_path = 'data/food-101/test'
+            
+            food_classes.sort()
+
+            if os.path.isdir(train_path) and os.path.isdir(test_path) and os.path.isdir(val_path):
+                print('train, val and test dir exist')
+                return(train_path,val_path,test_path)
+            else :
+                # Create directories for train and test
+                os.makedirs(train_path, exist_ok=True)
+                os.makedirs(test_path, exist_ok=True)
+                os.makedirs(val_path, exist_ok=True)
+
+
+
+                for food_class in food_classes:
+                    src_class_path = os.path.join(food_101_path, food_class)
+
+                    if os.path.isdir(src_class_path):
+                        # List all images in the class directory
+                        images = os.listdir(src_class_path)
+
+                        train_images = random.sample(images,750)  # First 750 for training
+                        remained_images = [item for item in images if item not in train_images]   # Remaining for testing
+                        val_image_number=int((val_pecentage*1000)/100)
+                        val_images=remained_images[:val_image_number]
+                        test_images=remained_images[val_image_number:]
+
+                        # Create class directories in train and test folders
+                        os.makedirs(os.path.join(train_path, food_class), exist_ok=True)
+                        os.makedirs(os.path.join(val_path, food_class), exist_ok=True)
+                        os.makedirs(os.path.join(test_path, food_class), exist_ok=True)
+
+                        # Copy training images
+                        for image_name in train_images:
+                            shutil.copy(os.path.join(src_class_path, image_name),
+                                        os.path.join(train_path, food_class))
+                        # Copy validation images
+                        for image_name in val_images:
+                            shutil.copy(os.path.join(src_class_path, image_name),
+                                        os.path.join(val_path, food_class))
+
+                        # Copy testing images
+                        for image_name in test_images:
+                            shutil.copy(os.path.join(src_class_path, image_name),
+                                        os.path.join(test_path, food_class))
+                        shutil.rmtree(src_class_path)
+
+                print("Train,val and test datasets created successfully.")
+
+                shutil.rmtree(food_101_path)
+                shutil.rmtree('data/food-101/meta')
+                os.remove("data/food-101/README.txt")
+                os.remove("data/food-101/license_agreement.txt")
+                os.remove("data/food-101.tar.gz")
+
+                return (train_path,val_path,test_path)
         
-        food_classes.sort()
+        else:
+            food_101_path='data/food-101/images'
+            train_path = 'data/food-101/train'
+            test_path = 'data/food-101/test'
+            
+            food_classes.sort()
 
-        if os.path.isdir(train_path) and os.path.isdir(test_path):
-            print('train, val and test dir exist')
-            return(train_path,test_path)
-        else :
-            # Create directories for train and test
-            os.makedirs(train_path, exist_ok=True)
-            os.makedirs(test_path, exist_ok=True)
+            if os.path.isdir(train_path) and os.path.isdir(test_path):
+                print('train, val and test dir exist')
+                return(train_path,test_path)
+            else :
+                # Create directories for train and test
+                os.makedirs(train_path, exist_ok=True)
+                os.makedirs(test_path, exist_ok=True)
 
 
 
-            for food_class in food_classes:
-                src_class_path = os.path.join(food_101_path, food_class)
+                for food_class in food_classes:
+                    src_class_path = os.path.join(food_101_path, food_class)
 
-                if os.path.isdir(src_class_path):
-                    # List all images in the class directory
-                    images = os.listdir(src_class_path)
+                    if os.path.isdir(src_class_path):
+                        # List all images in the class directory
+                        images = os.listdir(src_class_path)
+                        
+                        train_images = random.sample(images,750)  # First 750 for training
+                        test_images = [item for item in images if item not in train_images]   # Remaining for testing
+
+                        # Create class directories in train and test folders
+                        os.makedirs(os.path.join(train_path, food_class), exist_ok=True)
+                        os.makedirs(os.path.join(test_path, food_class), exist_ok=True)
+
+                        # Copy training images
+                        for image_name in train_images:
+                            shutil.copy(os.path.join(src_class_path, image_name),
+                                        os.path.join(train_path, food_class))
                     
-                    train_images = random.sample(images,750)  # First 750 for training
-                    test_images = [item for item in images if item not in train_images]   # Remaining for testing
+                        # Copy testing images
+                        for image_name in test_images:
+                            shutil.copy(os.path.join(src_class_path, image_name),
+                                        os.path.join(test_path, food_class))
+                        shutil.rmtree(src_class_path)
 
-                    # Create class directories in train and test folders
-                    os.makedirs(os.path.join(train_path, food_class), exist_ok=True)
-                    os.makedirs(os.path.join(test_path, food_class), exist_ok=True)
+                print("Train and test datasets created successfully.")
 
-                    # Copy training images
-                    for image_name in train_images:
-                        shutil.copy(os.path.join(src_class_path, image_name),
-                                    os.path.join(train_path, food_class))
-                   
-                    # Copy testing images
-                    for image_name in test_images:
-                        shutil.copy(os.path.join(src_class_path, image_name),
-                                    os.path.join(test_path, food_class))
-                    shutil.rmtree(src_class_path)
+                shutil.rmtree(food_101_path)
+                shutil.rmtree('data/food-101/meta')
+                os.remove("data/food-101/README.txt")
+                os.remove("data/food-101/license_agreement.txt")
+                os.remove("data/food-101.tar.gz")
 
-            print("Train and test datasets created successfully.")
-
-            shutil.rmtree(food_101_path)
-            shutil.rmtree('data/food-101/meta')
-            os.remove("data/food-101/README.txt")
-            os.remove("data/food-101/license_agreement.txt")
-            os.remove("data/food-101.tar.gz")
-
-            return (train_path,test_path)
+                return (train_path,test_path)
         
 
 
