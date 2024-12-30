f7fd6bee39726c6e5f46b62da9a14ec84ab28002
Ali Zare <alizare0502@gmail.com>
2024-12-30 12:58:34 +0330

    in swin_transformer.py check the existance of food101 dataset then download it.

--- swin_transformer.py
+++ swin_transformer.py
@@ -7,7 +7,11 @@ from torch.optim import Adam
 import engine
 import torch
 
-food_101_train_path,food_101_val_path,food_101_test_path=download_food101(root='data')
+
+if os.path.isdir('data/food-101'):
+    print("food101 dataset downloaded.")
+else:
+    food_101_train_path,food_101_val_path,food_101_test_path=download_food101(root='data')
 #create food25
 train_path,val_path,test_path=create_food25(root='data')
 
